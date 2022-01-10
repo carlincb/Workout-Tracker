1c076b7003f7b77ded4ec74257ecf38da05162ff
carlincb <carlin.colleen@gmail.com>
2022-01-09 22:04:26 -0700

    Added workout.js and index.js.

--- server.js
+++ server.js
@@ -8,4 +8,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout",
+{
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  }
+);
+
 app.use(routes);
+
+app.listen(PORT, () => { 
+    console.log("Server started.")
+});
